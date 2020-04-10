da60d82954d656699cbb5b6b6182dd019511edf9
masterful-array-3850 <crgammage@gmail.com>
2020-04-10 19:02:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ function takeANumber(katzDeli, name) {
   katzDeli.push(`${name}`);
   return (`Welcome ${name} you are number ${katzDeli.length} in line.`)
 }
+
+function nowServing(katzDeli) {
+  let i = 0;
+  do {katzDeli.slice(0), i++}
+  while (i < katzDeli)
+}
